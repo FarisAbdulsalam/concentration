013d692e71816a4c9eab0c08e2456ea2a14295ca
Faris <farisalmannaei@outlook.com>
2025-05-10 22:06:48 +0300

    added plan to JS file

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// plan
+
+// 1. declare all variables, constants, cached element references, and event listeners
+// constants: number of cards, array for the cards, winning score
+// variables: state of game(whether the game is started or not), current score, attempts, two variables to hold cards for comparison purposes
+// cached elements: start/reset button, the cards to click on, the win/lose messages
+// event listeners: click event listeners for the cards and start/reset buttons
+
+// 2. create an initialize function that resets all variables and readies the game for play. this function will be called upon
+// clicking the start button
+
+// 3. create a function that randomizes then renders all the cards on the screen 
+
+// 4. a function that flips the cards after they're clicked
+
+// 5. a function that compares two cards and adjusts attempts and score accordingly
