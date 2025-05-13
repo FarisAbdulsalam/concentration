31609a8d531bbd02c14a78ee547378baf60ede0e
Faris <farisalmannaei@outlook.com>
2025-05-13 23:25:14 +0300

    MVP reached

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-// plan
-
-// 1. declare all variables, constants, cached element references, and event listeners
-// constants: number of cards, array for the cards, winning score
-// variables: state of game(whether the game is started or not), current score, attempts, two variables to hold cards for comparison purposes
-// cached elements: start/reset button, the cards to click on, the win/lose messages
-// event listeners: click event listeners for the cards and start/reset buttons
-
-// 2. create an initialize function that resets all variables and readies the game for play. this function will be called upon
-// clicking the start button
-
-// 3. create a function that randomizes then renders all the cards on the screen 
-
-// 4. a function that flips the cards after they're clicked
-
-// 5. a function that compares two cards and adjusts attempts and score accordingly
-
 /*-------------- Constants -------------*/
 const emojiArr = ["🐏", "💕", "😊", "🐈"]
 const numOfCards = 8;
@@ -69,11 +52,9 @@ const play = (emoji) => {
     if (cardOne === '') {
         cardOne = emoji.target;
         emoji.target.classList.add('cardFront')
-        console.log('card one: ', cardOne);
     } else if (cardTwo === '') {
         cardTwo = emoji.target;
         emoji.target.classList.add('cardFront');
-        console.log('card two: ', cardTwo);
         compare(cardOne, cardTwo);
     }
     moves.innerHTML = `${attempts}/${maxAttempts} moves remaining`;
